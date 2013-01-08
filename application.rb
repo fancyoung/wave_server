c574023f1eb3f5bf3e69069b388229719f70823a
fancyoung <siyang1982@msn.com>
2013-01-08 14:57:51 +0000

    mongoid

--- application.rb
+++ application.rb
@@ -1,13 +1,18 @@
 require 'sinatra'
+require 'sinatra/reloader'
+require 'mongoid'
+
+require './model'
 
 get '/' do
   "Hello World!"
 end
 
 get '/news' do
-  "{posts: [
-    {'id': 1, 'title': 'test1', 'content': 'aaaaaaaaaa', 'link': 'http://t1.com'},
-    {'id': 2, 'title': 'test2', 'content': 'bbbbbbbbbb', 'link': 'http://t2.com'},
-    {'id': 3, 'title': 'test3', 'content': 'cccccccccc', 'link': 'http://t3.com'}
-  ]}"
+  Post.all().to_json
+#  '{"posts": [
+#    {"id": 1, "title": "test1", "content": "aaaaaaaaaa", "link": "http://t1.com"},
+#    {"id": 2, "title": "test2", "content": "bbbbbbbbbb", "link": "http://t2.com"},
+#    {"id": 3, "title": "test3", "content": "cccccccccc", "link": "http://t3.com"}
+#  ]}'
 end
